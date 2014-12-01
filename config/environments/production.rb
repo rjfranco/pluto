ef9d727acd00817b8d94625e6676112cbfb5bc55
Ramiro Jr. Franco <rjfranco@gmail.com>
2014-12-01 08:28:17 -0800

    Adding signup template, changing production value to reflect actual domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set url options
-  config.action_mailer.default_url_options = { host: 'pluto.ramiro.mx' }
+  config.action_mailer.default_url_options = { host: 'pluto.rocks' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
